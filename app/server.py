5616c6b543c35e5b3ebf415e7a55efa81303f004
Viswanath660 <viswanath660@gmail.com>
2019-07-05 20:15:33 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,8 +9,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1FJVNsbDUGs3Qt3vzOAZBurPYFk3v-Rt6'
+export_file_name = 'chexnet-auc-0862.pth'
 
 classes = ['black', 'grizzly', 'teddys']
 path = Path(__file__).parent
